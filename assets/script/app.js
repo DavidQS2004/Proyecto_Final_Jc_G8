4565ea076961b2d8c15089c9924ce97a269d1af0
David Quiñonez <davidesnaq@gmail.com>
2022-11-16 07:09:20 -0500

    se vincula el formulario a js y se ajusta el codigo css

--- assets/script/app.js
+++ assets/script/app.js
@@ -1,15 +1,12 @@
 const form = document.querySelector("#formulario-work-with-us");
-form.addEventListener('submit', handleSubmit);
+form.addEventListener("submit", handleSubmit);
 function handleSubmit(event) {
-  event.preventDefault()
+  event.preventDefault();
 }
-const nombre = 0;
-const telefono = 0;
-const email = 0;
 function retornarDatos() {
-  const nombre = document.getElementById("nombre").Value;
-  const telefono = document.getElementById("telefono").Value;
-  const email = document.getElementById("email").Value;
-  const datos = (nombre," ",telefono," ",email);
-  console.log(datos);
+  let nombreContacto = document.getElementById("nombre").Value;
+  let telefonoContacto = document.getElementById("telefono").Value;
+  let emailContacto = document.getElementById("email").Value;
+  let persona = (nombreContacto, " ", telefonoContacto, " ", emailContacto);
+  alert(persona);
 }
